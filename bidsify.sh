717b38518eb513c0a01d75ba4cb6965ae8067d08
Ryn Flaherty <ryn.flaherty@univ-amu.fr>
2026-01-08 14:59:23 +0100

    rename template bidsmap for clarity, add comments

--- bidsify.sh
+++ bidsify.sh
@@ -1,2 +1,6 @@
-bidsmapper ../test_dicoms/ bids/ -t ./bidsmap_dccn -n '*' -m '*' -a
+#first generate the bidsmap for the dicom data
+bidsmapper ../test_dicoms/ bids/ -t ./bidsmap_normabrain_template -n '*' -m '*' -a
+#edit the bidsmap manually in the GUI
+# bidseditor bids/ -t ./bidsmap_normabrain_template
+#then convert the dicoms to nifti and organize into BIDS structure using the bidsmap
 bidscoiner ../test_dicoms/ ./bids/
