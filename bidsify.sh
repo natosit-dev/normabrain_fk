54259bf2d30fc6b63eb5b6ab01492fe59f2f1ebd
Ryn Flaherty <ryn.flaherty@univ-amu.fr>
2026-01-19 11:47:45 +0100

    split copy_dicoms away from the rest of the workflow, but copy_dicoms still checks for new data each time workflow is called

--- bidsify.sh
+++ bidsify.sh
@@ -1,5 +1,5 @@
 #first copy the DICOMS to the project directory based on field strength
-python3 workflow/scripts/copy_dicoms_by_field_strength.py ../test_dicoms .
+python3 workflow/scripts/copy_dicoms_by_field_strength.py ../test_dicoms data/
 #generate the bidsmap for the dicom data
 bidsmapper ./3T/rawdata/dicoms/ ./3T/rawdata/bids/ -t ./config/bidsmap_normabrain_template -n '*' -m '*' -a
 #edit the bidsmap manually in the GUI
@@ -7,3 +7,6 @@ bidsmapper ./3T/rawdata/dicoms/ ./3T/rawdata/bids/ -t ./config/bidsmap_normabrai
 #then convert the dicoms to nifti and organize into BIDS structure using the bidsmap
 bidscoiner ./3T/rawdata/dicoms/ ./3T/rawdata/bids/
 python3 workflow/scripts/add_csa_data_to_meta.py ./3T/rawdata/bids/
+
+snakemake --config input_dicoms_path="../test_dicoms" --sdm conda --cores 2 copy_dicoms_by_field_strength
+snakemake --config input_dicoms_path="../test_dicoms" --sdm conda --cores 2
