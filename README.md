c0eea17982eac63355af61b331e274bc3c71d869
Ryn Flaherty <rf2485@nyu.edu>
2026-03-10 14:35:12 +0100

    snakemake.yaml did not need to change, channel priority just needed to be set to strict

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ This repository utilizes git submodules. To clone, use the command git clone --r
 Requires: python3, conda, singularity, gcc
 
 Installation: conda env create -f workflow/envs/snakemake.yaml
+If installation from snakemake.yaml leads to conflicts, try running `conda config --set channel_priority strict` first
--- run_workflow.sh
+++ run_workflow.sh
@@ -3,7 +3,7 @@ snakemake --config input_dicoms_path="../test_dicoms" -np data/derivatives/3T/MP
 # snakemake --config input_dicoms_path="../test_dicoms" --sdm conda --cores 2 results/add_csa_data_to_meta_3T.complete
 
 if command -v nvidia-smi; then
-    snakemake --config input_dicoms_path="/home/rflaherty/test_dicoms" --sdm conda apptainer --cores 8 --singularity-args "--nv -e"
+    snakemake --config input_dicoms_path="/home/rflaherty/test_dicoms" --sdm conda apptainer --cores 8 --singularity-args "--nv -e" --rerun-incomplete
 else
-    snakemake --config input_dicoms_path="../test_dicoms" --sdm conda apptainer --cores 8
+    snakemake --config input_dicoms_path="../test_dicoms" --sdm conda apptainer --cores 8 --rerun-incomplete
 fi
