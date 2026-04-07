4ac97ab86372b6b3336f628ec434a6fccb741415
Ryn Flaherty <rf2485@nyu.edu>
2026-04-07 18:02:15 +0200

    split bidsify and the rest of the workflow into two different calls to snakemake

--- run_workflow.sh
+++ run_workflow.sh
@@ -1,6 +1,6 @@
 snakemake --config input_dicoms_path="../test_dicoms" -np data/derivatives/3T/MPM/sub-rfl260123normanoel/ses-1/sub-rfl260123normanoel_ses-1_acq-vibeMT_T1map_registeredtoMP2RAGE.nii.gz --dag | sed -n "/digraph/,/}/p" | dot -Tsvg > qT1_dag.svg
 
-# snakemake --config input_dicoms_path="../test_dicoms" --sdm conda --cores 2 results/add_csa_data_to_meta_3T.complete
+snakemake --config input_dicoms_path="../test_dicoms" --sdm conda --cores 2 data/rawdata/bids/3T/code/bidscoin/fixmeta.log
 
 if command -v nvidia-smi; then
     snakemake --config input_dicoms_path="/DATA_CEMEREM/data/users/ttroalen/vida/normadev/" protocol_path="/DATA_CNS/PROJECTS/NORMABRAIN/NORMA_DEV_CPP/" --resources mem_mb=10000 --sdm conda apptainer --cores 8 --singularity-args "--nv -e" --rerun-incomplete
